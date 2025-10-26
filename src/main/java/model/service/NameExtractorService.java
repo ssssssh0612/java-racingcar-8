5ef85633842f28b09a3f08d09e6ee71505334d3a
sibjagun <sibjagun99@kyungmin.ac.kr>
2025-10-26 10:31:32 +0900

    feat(service): "입력받은 문자열로부터 RacingCars를 생성하는 서비스 완성"

---
+++ src/main/java/model/service/NameExtractorService.java
@@ -0,0 +1,24 @@
+package model.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import model.domain.CarName;
+import model.domain.Distance;
+import model.domain.RacingCar;
+import model.domain.RacingCars;
+import model.domain.UserInputString;
+
+public class NameExtractorService {
+    private static final int ZERO = 0;
+
+    public RacingCars extractStringReturnRacingCars(UserInputString userInputString) {
+        Iterator<String> names = userInputString.getNameIterator();
+        List<RacingCar> racingCars = new ArrayList<>();
+        while (names.hasNext()) {
+            String name = names.next();
+            racingCars.add(new RacingCar(new CarName(name), new Distance(ZERO)));
+        }
+        return new RacingCars(racingCars);
+    }
+}
