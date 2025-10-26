8c50cbe1adc8cde02f27dc7b75ebd7b9a366b629
sibjagun <sibjagun99@kyungmin.ac.kr>
2025-10-26 10:39:20 +0900

    feat: "메인 로직 생성 완성"

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import model.domain.RacingCars;
+import model.domain.UserInputNumber;
+import model.domain.UserInputString;
+import model.domain.Winners;
+import model.service.NameExtractorService;
+import model.service.RacingCarsService;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        UserInputString userInputString = new UserInputString(inputView.getUserInputString());
+        NameExtractorService nameExtractorService = new NameExtractorService();
+        RacingCars racingCars = nameExtractorService.extractStringReturnRacingCars(userInputString);
+        OutputView outputView = new OutputView();
+        RacingCarsService racingCarsService = new RacingCarsService(outputView);
+        UserInputNumber userInputNumber = new UserInputNumber(inputView.getUserInputNumber());
+        RacingCars movedRacingCars = racingCarsService.moveAndLogSnapshots(userInputNumber, racingCars);
+        Winners winnerRacingCars = movedRacingCars.findWinners();
+        outputView.showOutput(winnerRacingCars.toResult());
     }
 }
